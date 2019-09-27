f3f3a6c045e731ab6d926dde1e4eeae87a7661d7
Rémy <Remypottierpro@gmail.com>
2019-09-27 11:13:04 +0200

    Finish style of feature

--- function.js
+++ function.js
@@ -1,4 +1,5 @@
 //// Moving plate 
+
 let scroll = 0
 let barWidth = 0
 
@@ -17,9 +18,9 @@ document.addEventListener("scroll", function () {
     barWidth = scroll * ratio
     document.querySelector('.bar').style.width = barWidth + "px";
     if (barWidth <= 0) {
-        document.querySelector('.fa-hamburger').style.marginLeft = barWidth + "px";
+        document.querySelector('.fa-hamburger').style.marginLeft = barWidth+10 + "px";
     } else if (barWidth >= width - 25) {
-        document.querySelector('.fa-hamburger').style.marginLeft = barWidth - 35 + "px";
+        document.querySelector('.fa-hamburger').style.marginLeft = barWidth - 40 + "px";
     } else {
         document.querySelector('.fa-hamburger').style.marginLeft = barWidth - 15 + "px";
     }
